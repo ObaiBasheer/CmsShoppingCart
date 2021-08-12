6640623b9ae043110d1846e92ea5dd8b5635e680
obai-hup <obai2015@yahoo.com>
2021-08-12 11:54:34 +0200

    Add project files.

---
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,21 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext: DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Page> Pages { set; get; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
+    }
+}
---
+++ CmsShoppingCart/Models/Page.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class Page
+    {
+        public int Id { set; get; }
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        public string Title { set; get; }
+        public string Slug { set; get; }
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Content { set; get; }
+        public int Sorting { set; get; }
+    }
+}
